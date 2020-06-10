37ef131c5c8c79b856c861b3946299cbb864fe39
NNHieu <ngochieutb13@gmail.com>
2020-06-10 15:09:50 +0700

    detail page

--- webpack.dev.js
+++ webpack.dev.js
@@ -66,6 +66,20 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.md$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    },
+                    {
+                        loader: "markdown-loader",
+                        options: {
+                            /* your options here */
+                        }
+                    }
+                ]
             }
         ],
     },
