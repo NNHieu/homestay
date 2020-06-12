4535e7df8a72449c55349d02b84ab116fb94bc6d
NNHieu <ngochieutb13@gmail.com>
2020-06-12 10:03:59 +0700

    page detaul

---
+++ frontend/src/components/detail/FacilitiesList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
+import DoneIcon from '@material-ui/icons/Done';
+import { facilties_list } from '../../reducers/upload';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(0.5),
+        },
+    },
+}));
+
+export default function FacilitiesList(props) {
+    const classes = useStyles();
+
+
+    const handleDelete = () => {
+        console.info('You clicked the delete icon.');
+    };
+
+    const handleClick = () => {
+        console.info('You clicked the Chip.');
+    };
+
+    return (
+        <div className={classes.root}>
+            {props.facilities.map(
+                (f, index) =>
+                    f && <Chip key={index} label={facilties_list.basicFacilities.list[index].title} />
+            )}
+        </div>
+    );
+}
--- frontend/src/components/detail/Main.js
+++ frontend/src/components/detail/Main.js
@@ -6,7 +6,16 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Markdown from './Markdown';
 import CloudinaryGallery from '../general/CloudinaryGallery'
-import { Paper } from '@material-ui/core';
+import { Paper, Button, Link, Box } from '@material-ui/core';
+import MapComponent from '../general/MapComponent';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import FacilitiesList from './FacilitiesList';
+import HomeWorkIcon from '@material-ui/icons/HomeWork';
 
 const useStyles = makeStyles((theme) => ({
   markdown: {
@@ -15,21 +24,71 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function Main(props) {
   const classes = useStyles();
   const { posts, title, address } = props;
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <Grid item xs={12} md={8}>
-      <CloudinaryGallery images={['wlzzofdp8dzcne645xxz', 'gtf0c9wtgmvbooarthxu', 'ett1wcbrnbpocgkwu10b']} />
       <Paper variant="outlined" style={{ padding: '10px', marginBottom: '20px' }}>
         <Typography variant="h4">
           {title}
         </Typography>
         <Typography variant="subtitle1" color="textSecondary">
-          {address}
+          {address} - <Link href="#" onClick={handleClickOpen} >Xem trên bản đồ </Link>
         </Typography>
       </Paper>
+      <Paper variant="outlined" style={{ padding: "20px 20px", marginBottom: "20px" }}>
+        <Box style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center"
+        }}>
+          <HomeWorkIcon style={{}} fontSize="large" />
+          <Typography>Nhà/Chung cư</Typography>
+        </Box>
+      </Paper>
+      <CloudinaryGallery images={['wlzzofdp8dzcne645xxz', 'gtf0c9wtgmvbooarthxu', 'ett1wcbrnbpocgkwu10b']} />
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+        maxWidth="lg"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Đây là vị trí homestay trên bản đồ"}</DialogTitle>
+        <DialogContent
+          style={{
+            width: "1000px"
+          }}
+        >
+          <Box style={{
+            width: "1000px"
+          }}>
+            <MapComponent lat={21.0227788} lng={105.8194541} zoom={16} />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Chỉ đường
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <FacilitiesList facilities={[true, false, true, false, true, false, true, false]} />
       <Typography>
         Tọa lạc tại thành phố Hà Nội, gần Nhà Hát Lớn và trung tâm thương mại Tràng Tiền Plaza, omfy and Cozy HomeSTAY nearby Hanoi Opera House cung cấp chỗ nghỉ với WiFi cùng chỗ đỗ xe riêng miễn phí.
 
